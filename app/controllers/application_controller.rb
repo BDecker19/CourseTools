edc7c35d36d9d56f1d7586a56e194c216a44a739
Benji Decker <bensdecker@gmail.com>
2013-04-22 06:11:23 -0400

    working on login
    think it works, but is isolated to sessions areas.  tryign to modify to
    make work everywhere

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,36 @@ class ApplicationController < ActionController::Base
   	protect_from_forgery
 	
 	protected 
-			def authenticate_user
-				unless session[:user_id]
-					redirect_to(:controller => 'sessions', :action => 'login')
-					return false
-				else
-					# set current user object to @current_user object variable
-					@current_user = User.find session[:user_id] 
-					return true
-				end
+		def authenticate_user
+			unless session[:user_id]
+				redirect_to(:controller => 'sessions', :action => 'login')
+				return false
+			else
+				# set current user object to @current_user object variable
+				@current_user = User.find session[:user_id] 
+				return true
 			end
+		end
 
-			def save_login_state
-				if session[:user_id]
-					redirect_to(:controller => 'sessions', :action => 'home')
-					return false
-				else
-					return true
-				end
+		def save_login_state
+			if session[:user_id]
+				redirect_to(:controller => 'sessions', :action => 'home')
+				return false
+			else
+				return true
 			end
+		end
+
+
+  private
+ 
+  # Finds the User with the ID stored in the session with the key
+  # :current_user_id This is a common way to handle user login in
+  # a Rails application; logging in sets the session value and
+  # logging out removes it.
+  def current_user
+    @_current_user ||= session[:current_user_id] &&
+      User.find_by_id(session[:current_user_id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130421230032) do
+ActiveRecord::Schema.define(:version => 20130422092744) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -38,12 +38,12 @@ ActiveRecord::Schema.define(:version => 20130421230032) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
     t.string   "email"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.string   "encrypted_password"
     t.string   "salt"
+    t.string   "username"
   end
 
   create_table "votes", :force => true do |t|
