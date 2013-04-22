2649a1ce71ea412bed1a1a9873867fd4906e63f0
Benji Decker <bensdecker@gmail.com>
2013-04-22 06:15:07 -0400

    got it!
    sweet!  think i nailed that 'ish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   	protect_from_forgery
 	
+	before_filter :authenticate_user, :except => [:login]
+	before_filter :save_login_state, :only => [:login, :login_attempt]
+
 	protected 
 		def authenticate_user
 			unless session[:user_id]
