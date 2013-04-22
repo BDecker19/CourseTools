25aad081a31f21536c310ef800104287cd6f4468
Benji Decker <bensdecker@gmail.com>
2013-04-22 06:18:53 -0400

    jk :(
    seems like not working… gonna try rolling back?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
-	before_filter :authenticate_user, :only => [:home, :profile, :setting]
-	before_filter :save_login_state, :only => [:login, :login_attempt]
-
+	# originals -- seemed like only applied to session variables
+	# before_filter :authenticate_user, :only => [:home, :profile, :setting]
+	# before_filter :save_login_state, :only => [:login, :login_attempt]
 
   	def login
 		render :login
